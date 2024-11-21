f45c53b45250a9c91e3a0b460d850c64a276ba53
David Murdoch <dsmurdoch357@gmail.com>
2024-11-21 20:21:33 +0000

    chore(shadcn): add tailwindcss and shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
@@ -5,4 +6,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [deno(), react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./src"),
+    },
+  },
 });
