9d1fd271060348ba3f3c335b0ac988e31eeeac30
David Murdoch <dsmurdoch357@gmail.com>
2025-03-24 14:39:54 +0000

    chore(deps): update third-party dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "node:path";
 import { defineConfig } from "vite";
 import deno from "@deno/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [deno(), react()],
+  plugins: [deno(), react(), tailwindcss()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./src"),
